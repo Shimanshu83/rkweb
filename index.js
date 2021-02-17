a1d3472a959fb848d59f8212c19deeaf1711d93d
Shimanshu yadav <shimanshuyadav83@gmail.com>
2021-02-17 23:37:47 +0530

    third commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-let menu = document.getElementsByClassName("menu") ; 
+let menu = document.getElementById("menu1");
+menu.addEventListener("click", () =>  {
+    let options = document.getElementById("menuActivated");
 
- 
+    if (options.style.display == "flex"){
+        options.style.display = "none" ; 
+
+    }
+    else {
+        options.style.display = "flex" ; 
+
+    }
+    
+    
+
+} )
